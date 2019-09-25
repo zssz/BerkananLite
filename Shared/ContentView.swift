81f44cb04c0b76eb188d54cf6f94549bdd8393cf
Zsombor Szabo <zsombor@gmail.com>
2019-09-25 08:44:01 +0200

    Add stringsdict Remove bug workaround for double presentation of alert because it is fixed in iOS 13.1

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -21,12 +21,6 @@ struct ContentView : View {
             self.messagesView.navigationBarTitle(Text("Public")).navigationBarItems(
                 leading: Button(action: {
                     self.showsInfo = true
-                    // Bug workaround: This fixes the double presentation issue of the alert from a bar button item
-                    if UIDevice.current.userInterfaceIdiom == .phone {
-                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+0.2) {
-                            self.showsInfo = false
-                        }
-                    }
                 }, label: { Image(systemName: "info.circle").font(.system(size: .init(integerLiteral: 17))).imageScale(.large).padding(5) }).alert(isPresented: $showsInfo) {
                     Alert(title: Text("About"), message: Text("📢 Broadcast messages to the crowd around you. The range is about 70 meters, but your messages can reach further because they are retransmitted by receiving apps.\n\n🚨 We believe this service is essential, especially in emergencies. It's enabled by apps joining forces using BerkananSDK.\n\n#berkanansdk #opensource #bluetooth #offline #network #public #broadcast #text #message #messaging #chat #crowd #nearby #background #privacy #noaccount #anonymous"), primaryButton: .cancel(), secondaryButton: .default(Text("Learn More"), action: {
                         guard let url = URL(string: "https://github.com/zssz/BerkananSDK") else { return }
